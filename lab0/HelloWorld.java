eb7d7a0d5ff9065595e9c76b97b2623365d5e938
Kori <6930613017@psu.ac.th>
2026-09-14 15:45:44 +0700

    add HelloWorld

--- lab0/HelloWorld.java
+++ lab0/HelloWorld.java
@@ -1,6 +1,7 @@
 package lab0; 
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.print("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hi Nalin");
     }
 }
